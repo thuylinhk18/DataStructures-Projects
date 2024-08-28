ca7ebd15518409dd7ae44d8c413e2d6a36944d95
Pham Thi Thuy Linh <thuylinhcua2024@gmail.com>
2024-08-28 08:36:24 +0700

    Delete messages to keep the validation logic separate from the UI logic

--- src/SinglyLinkedList_BookCatalog/BookCatalog.java
+++ src/SinglyLinkedList_BookCatalog/BookCatalog.java
@@ -36,13 +36,11 @@ public class BookCatalog {
 
         // Check if the length of ISBN is either 10 or 13
         if (toCharArray.length != 13 && toCharArray.length != 10) {
-            System.out.println("ISBN must have 10 or 13 digits!");
             return false;
         }
         // Check if all characters are digits
         for (char c : toCharArray) {
             if (!Character.isDigit(c)) {
-                System.out.println("ISBN's characters must be digits!");
                 return false;
             }
         }
